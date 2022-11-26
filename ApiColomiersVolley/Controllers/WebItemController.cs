f0edc09709a18de905e843d9494d541b53f740c1
Martial VAUTIER <mvautier@idaia.group>
2022-11-26 01:21:56 +0100

    fix: route webitem

--- ApiColomiersVolley/Controllers/WebItemController.cs
+++ ApiColomiersVolley/Controllers/WebItemController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ApiColomiersVolley.Controllers
 {
     [ApiController]
-    [Route("api")]
+    [Route("api/[controller]")]
     public class WebItemController : ControllerBase
     {
         private readonly ILogger<WebItemController> _logger;
