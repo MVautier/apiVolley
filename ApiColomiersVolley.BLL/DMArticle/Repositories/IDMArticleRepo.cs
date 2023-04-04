be7c5496d992b780223eb66a2cee11300becc2ea
Martial VAUTIER <mvautier@idaia.group>
2023-04-04 09:23:43 +0200

    feat: article

---
+++ ApiColomiersVolley.BLL/DMArticle/Repositories/IDMArticleRepo.cs
@@ -0,0 +1,14 @@
+﻿using ApiColomiersVolley.BLL.DMArticle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiColomiersVolley.BLL.DMArticle.Repositories
+{
+    public interface IDMArticleRepo
+    {
+        Task<List<DtoArticle>> Get();
+    }
+}
